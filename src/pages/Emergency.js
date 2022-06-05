95c1e4c611fd6584936378b6801802baf2c51c84
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-06-05 15:57:26 -0700

    Arreglando leaftlet container responsive (intento)

--- src/pages/Emergency.js
+++ src/pages/Emergency.js
@@ -24,113 +24,103 @@ import Popup from './Popup'
 import GetVets from './functions/GetVets'
 
 const Emergency = () => {
-    const [vets, setVets] = useState([])
-    const [selectedVet, setSelectedVet] = useState({
-        name: 'N/A',
-        direction: 'N/A',
-        phone: 'N/A',
-    })
-    const [loaded, setLoaded] = useState(false)
-    const [seePopup, setSeePopup] = useState(false)
+  const [vets, setVets] = useState([])
+  const [selectedVet, setSelectedVet] = useState({
+    name: 'N/A',
+    direction: 'N/A',
+    phone: 'N/A',
+  })
+  const [loaded, setLoaded] = useState(false)
+  const [seePopup, setSeePopup] = useState(false)
 
-    React.useEffect(() => {
-        ;(async () => {
-            const data = await GetVets()
-            if (!data['success']) {
-                alert(data['error'])
-            } else {
-                setVets(data['data'])
-                setLoaded(true)
-            }
-        })()
-    }, [])
+  React.useEffect(() => {
+    ;(async () => {
+      const data = await GetVets()
+      if (!data['success']) {
+        alert(data['error'])
+      } else {
+        setVets(data['data'])
+        setLoaded(true)
+      }
+    })()
+  }, [])
 
-    const SeeMaps = ({}) => {
-        return (
-            <>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1,maximum-scale=1,user-scalable=no"
-                />
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
-                <>
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1.0"
-                    ></meta>
-                    <div
-                         className={
-                            selectedVet['name'] !== 'N/A'
-                                ? 'mapNpopDiv'
-                                : 'mapNpopDiv2'
-                        }
-                    >
-                    
-                        <div
-                             className={
-                                selectedVet['name'] !== 'N/A'
-                                    ? 'mapNpopWrap'
-                                    : 'mapNpopWrap2'
-                            }
-                        >
-                            
-                        {selectedVet['name'] !== 'N/A' && (
-                            <div class="displayInfo">
-                                <h2>Información</h2>
-                                <div class="vetInfo">
-                                    <h4>Nombre: <p>{selectedVet['name']}</p></h4>
-                                    <h4>Dirección: <p>{selectedVet['direction']}</p></h4>
-                                    <h4>
-                                        Número de teléfono: <p>{selectedVet['phone']}</p>
-                                    </h4>
-                                </div>
-                                <button
-                                    class="emmBtn"
-                                    onClick={() => setSeePopup(true)}
-                                >
-                                    Ver Más Detalles
-                                </button>
-                            </div>
-                        )}
-                            <div
-                                className={
-                                    selectedVet['name'] !== 'N/A'
-                                        ? 'displayMap'
-                                        : 'displayMap-ext'
-                                }
-                            >
-                                <div
-                                    className={
-                                        selectedVet['name'] !== 'N/A'
-                                            ? 'map-container'
-                                            : 'map-container-ext'
-                                    }
-                                >
-                                    {!loaded && <h1>Cargando...</h1>}
-                                    {loaded && (
-                                        <MapComponent
-                                            vets={vets}
-                                            setSelectedVet={setSelectedVet}
-                                        />
-                                    )}
-                                </div>
-                            </div>
-                            </div>
-                    </div>
-                </>
-            </>
-        )
-    }
-
+  const SeeMaps = ({}) => {
     return (
+      <>
+        <meta
+          name="viewport"
+          content="initial-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
         <>
-            {!seePopup && <SeeMaps />}
-            {seePopup && (
-                <Popup vet={selectedVet} regretOriginal={setSeePopup} />
-            )}
+          <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+          <div
+            className={
+              selectedVet['name'] !== 'N/A' ? 'mapNpopDiv' : 'mapNpopDiv2'
+            }
+          >
+            <div
+              className={
+                selectedVet['name'] !== 'N/A' ? 'mapNpopWrap' : 'mapNpopWrap2'
+              }
+            >
+              {selectedVet['name'] !== 'N/A' && (
+                <div class="displayInfo">
+                  <h2>Información</h2>
+                  <div class="vetInfo">
+                    <h4>
+                      Nombre: <p>{selectedVet['name']}</p>
+                    </h4>
+                    <h4>
+                      Dirección: <p>{selectedVet['direction']}</p>
+                    </h4>
+                    <h4>
+                      Número de teléfono: <p>{selectedVet['phone']}</p>
+                    </h4>
+                  </div>
+                  <button class="emmBtn" onClick={() => setSeePopup(true)}>
+                    Ver Más Detalles
+                  </button>
+                </div>
+              )}
+              <div
+                className={
+                  selectedVet['name'] !== 'N/A'
+                    ? 'displayMap'
+                    : 'displayMap-ext'
+                }
+              >
+                <div
+                  className={
+                    selectedVet['name'] !== 'N/A'
+                      ? 'map-container'
+                      : 'map-container-ext'
+                  }
+                >
+                  {!loaded && <h1>Cargando...</h1>}
+                  {loaded && (
+                    <MapComponent vets={vets} setSelectedVet={setSelectedVet} />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </>
+      </>
     )
+  }
+
+  return (
+    <>
+      {!seePopup && <SeeMaps />}
+      {seePopup && <Popup vet={selectedVet} regretOriginal={setSeePopup} />}
+    </>
+  )
 }
 
 export default Emergency
